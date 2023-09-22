f37c53928d39772cdb29dc1879b8166104db4608
creategamer <144140289+creategamer@users.noreply.github.com>
2023-09-22 14:42:25 +0000

    conversion of datatype

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,34 @@
+let score="33abc"
+
+// let score=33
+// let score="33"
+//let score="muzammil"=>NaN
+// let score=null=>0
+// let score=undefined=>Nan
+//let score=true=>0 or 1
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+
+let isLoggedIn=1//="" false, "abc" true
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
