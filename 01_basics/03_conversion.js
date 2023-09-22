bdf49dbd8f1ddf079d12cf99833ddf8173e89ee3
creategamer <144140289+creategamer@users.noreply.github.com>
2023-09-22 19:26:39 +0000

    3 file change conversion and datatype summary

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -32,3 +32,26 @@ let stringNumber=String(someNumber)
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+//   **********************operation**********************************
+
+let value=3
+let negvalue=-value
+
+console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%2);
+
+
+let str1="hello"
+let str2="hitesh"
+
+let str3=str1+str2
+console.log(str3);
---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,40 @@
+//primitive
+
+//7 types : string,Number,Boolean,null,undefined,symbol,BigInt
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+
+let userEmail=undefined;
+let useremail;
+
+const id=Symbol('123')
+const anotherid=Symbol('123')
+
+console.log(id==anotherid);
+
+const bignumber=333333333333333333333333n
+
+
+
+
+//Reference type(Non primitive)
+
+//Array,Objects,Functions,
+
+
+const heros=["shaktiman","naagraj","doga"]
+
+let obj={
+    names:"muzamil",
+    age:21
+}
+
+const myfunction=function(){
+    console.log("Hello world")
+}
+
+console.log(typeof bignumber)
