2b204e63c5f2ae295d68e4aebcfc046f9674d9f5
creategamer <144140289+creategamer@users.noreply.github.com>
2023-09-25 19:11:48 +0000

    maths and nums 26/9/2023

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,4 +1,42 @@
-const score=400
+// const score=400
+// console.log(score);
 
-const balance=new Number(100)
-console.log(balance);
+
+// const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const otherNumber=123.8545223
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hundrad=10000000
+// console.log(hundrad.toLocaleString('en-in'));
+
+
+// //***************************************************************maths */
+
+// console.log(Math);
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(5.62))
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4,5,6,8))
+// console.log(Math.max(4,5,6,7,8));
+
+// //mostly used in maths library
+// console.log(Math.random());
+
+// console.log((Math.random()*10)+1);
+
+const min=1
+const max=10
+
+console.log(Math.round(Math.random(min,max)*10))//self
+
+console.log(Math.floor(Math.random() * (max - min +1))+min)//write
+
+
